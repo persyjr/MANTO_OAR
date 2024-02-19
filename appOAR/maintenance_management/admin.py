ddd546c36635ab1f5c905f78d6c1952db45841af
Alejandro Delgado <Edelgado@wigilabs.com>
2024-02-19 00:59:17 -0500

    'Work order and tool model'

--- appOAR/maintenance_management/admin.py
+++ appOAR/maintenance_management/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from maintenance_management import models as m
 # Register your models here.
+
+@admin.register(m.WorkOrder)
+class CategoriaAdmin(admin.ModelAdmin):
+    readonly_fields=('created','consecutive')
+
+@admin.register(m.Tools)
+class Tools(admin.ModelAdmin):
+    readonly_fields=('model','usernumber')
--- appOAR/maintenance_management/models.py
+++ appOAR/maintenance_management/models.py
@@ -2,12 +2,48 @@ from django.db import models
 from django.conf import settings
 # Create your models here.
 
-class OrdenDeTrabajo(models.Model):
-    solicitante=models.ForeignKey(settings.AUTH_USER_MODEL, related_name='+', on_delete=models.CASCADE)# Preguntar si dejar configurado on_Delete para eliminar todo el historial 
-    fecha_Creacion=models.DateTimeField(auto_now_add=True)
+class TypeMaintenance(models.TextChoices):
+    PREVENTIVO = 'PM', 'Preventivo'
+    CORRECTIVO = 'CM', 'Correctivo'
+    OVERHAULL = 'OV', 'Overhaull'
+
+class Tools(models.Model):
+    typeMaintenance = models.CharField(max_length=10, choices=TypeMaintenance.choices)
+    name = models.CharField(max_length=200, blank=True)
+    description = models.CharField(max_length=200, blank=True)
+    brand = models.CharField(max_length=200, blank=True)
+    model = models.CharField(max_length=200, blank=True)
+    usernumber = models.CharField(max_length=200, blank=True)
+    receiptInspection = models.CharField(max_length=200, blank=True)
     
     def __str__(self):
-        return "OrdenDeTrabajo: %s creacion: %s "%(self.tipo,self.fecha_Creacion)
+        return "Tools: %s name: %s "%(self.name,self.model)
     class Meta:
-        verbose_name='orden_de_trabajo'
-        verbose_name_plural='ordenes_de_trabajo'
+        verbose_name='tool'
+        verbose_name_plural='tools'
+
+class StateEnum(models.TextChoices):
+    RECIBIDO = 'RC', 'Recibido'
+    MANTENIMIENTO = 'MTO', 'En Mantenimiento'
+    ENTREGADO = 'EN', 'Entregado'
+
+class WorkOrder(models.Model):
+    name = models.CharField(max_length=200, blank=True)
+    consecutive = models.CharField(max_length=200, blank=True)
+    state = models.CharField(max_length=10, choices=StateEnum.choices)
+    userApplicant = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='+', on_delete=models.CASCADE)
+    created=models.DateTimeField(auto_now_add=True)
+    endDate=models.DateTimeField(auto_now_add=True)
+    tools=models.ManyToManyField(Tools)
+    place = models.CharField(max_length=200, blank=True)
+    
+    def __str__(self):
+        return "WORK ORDER: %s init Date: %s "%(self.name,self.created)
+    class Meta:
+        verbose_name='work_order'
+        verbose_name_plural='work_orders'
+
+
+
+    
+    
